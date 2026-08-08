1bec866ea67f22e054bf9aa57db548a9e6ec41d0
marquezchristina467-maker <marquezchristina467@gmail.com>
2026-08-08 00:14:10 -0700

    Create Home component with basic structure

---
+++ src/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 24, fontFamily: "sans-serif" }}>
+      <h1>TaxAid</h1>
+      <p>Your app is running.</p>
+    </div>
+  );
+}
