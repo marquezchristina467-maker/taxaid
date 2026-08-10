b88c6a212e3e36b68b32eb27f5bc111a42f1bd7f
marquezchristina467-maker <marquezchristina467@gmail.com>
2026-08-10 05:19:38 -0700

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -2,5 +2,4 @@ import { createRoot } from 'react-dom/client'
 import './shadcn.css'
 import App from './App'
 
-const root = createRoot(document.getElementById('app')!)
-root.render(<App />)
+const root = createRoot(document.getElementById('root')!)
